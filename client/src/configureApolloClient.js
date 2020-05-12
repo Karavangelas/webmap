ca0ef35ee9d6d80c1ca75a9f3fdc4944f8195021
Dan Dunkers <ddunkers@brytemap.com>
2020-05-12 14:20:43 -0400

    Server Issues

--- client/src/configureApolloClient.js
+++
@@ -1,5 +0,0 @@
-import { ApolloClient } from 'apollo-client';
-
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-export const client = new ApolloClient({ link: [], cache: new InMemoryCache() });
--- client/src/containers/App/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
-
-import Countries from '../Map/countries';
-const App = () => {
-  return <>Hello World</>;
-};
-
-export default hot(module)(App);
