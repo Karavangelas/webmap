6ad025836ebd9a71446426427a5f7691b3b8dc46
les324 <les324@coe-svl-ecestu2.coe.drexel.edu>
2020-06-07 17:55:13 -0400

    Added argparsing as well as verification for mongo document existing and only adding if new

--- my_scripts/restruct_region_pie_chart.py
+++ my_scripts/restruct_region_pie_chart.py
@@ -1,17 +1,27 @@
 import pathlib
 import json
 from pymongo import MongoClient
+import argparse
 
-# get file, temp hardcoded
-p =  pathlib.Path("region_pie_chart.json")
+def main():
+    parser = argparse.ArgumentParser(description="Import region pie chart data to MongoDB")
+    parser.add_argument("--file", dest="file", help="Path to region_pie_chart.json", required=True)
+    args = parser.parse_args()
+    # get file, temp hardcoded
+    p =  pathlib.Path(args.file)
+    
+    if not p.exists():
+        print("File DNE")
+        return 1
+    
+    f = json.loads(p.read_text())
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.gisaid
+    
+    for country, ism in f.items():
+        temp = {"country": country, country: ism}
+        db.regionPieChart.update(temp, temp, upsert=True})
+    return 0
 
-if not p.exists():
-    print("File DNE")
-
-f = json.loads(p.read_text())
-client = MongoClient("mongodb://localhost:27017")
-db = client.gisaid
-
-for country, ism in f.items():
-    temp = {"country": country, country: ism}
-    db.regionPieChart.insert_one(temp)
+if __name__ == "__main__":
+    main()
--- my_scripts/restruct_region_time_chart.py
+++ my_scripts/restruct_region_time_chart.py
@@ -1,17 +1,27 @@
 import pathlib
 import json
 from pymongo import MongoClient
+import argparse
 
-# get file, temp hardcoded
-p =  pathlib.Path("region_time_series.json")
+def main():
+    parser = argparse.ArgumentParser(description="Import region time series data to MongoDB")
+    parser.add_argument("--file", dest="file", help="Path to region_time_series.json", required=True)
+    args = parser.parse_args()
+    # get file, temp hardcoded
+    p =  pathlib.Path(args.file)
 
-if not p.exists():
-    print("File DNE")
+    if not p.exists():
+        print("File DNE")
+        return 1
+    f = json.loads(p.read_text())
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.gisaid
+    
+    for time, ism in f.items():
+        temp = {"date": time, time: ism}
+        db.regionTimeSeries.update(temp, temp, upsert=True)
+    return 0
 
-f = json.loads(p.read_text())
-client = MongoClient("mongodb://localhost:27017")
-db = client.gisaid
+if __name__ == "__main__":
+    main()
 
-for time, ism in f.items():
-    temp = {"date": time, time: ism}
-    db.regionTimeSeries.insert_one(temp)
